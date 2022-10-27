75c8380adb1ababd3b57169c9f80fcc5517fd0f5
shreyaa2798 <112149651+shreyaa2798@users.noreply.github.com>
2022-10-27 15:35:10 -0500

    Election file

--- code/election_2020.py
+++ code/election_2020.py
@@ -8,29 +8,59 @@ OUTPUT_PATH = os.path.join(OUTPUT_DIR, "election_report.csv")
 
 def count_votes(path):
 
-    # you write this
+    with open(IN_PATH, "r") as in_file:
+        counts = {}
+
+        dict_reader = csv.DictReader(in_file)
+
+        for row in dict_reader:
+            year = row["year"]
+            state = row["state_po"]
+            candidate = row["candidate"]
+            votes = row["candidatevotes"]
+            mode = row["mode"]
+
+            key = (year, state, candidate)
+            
+
+            if not votes == "NA" and year == "2020":
+                votes_int = int(votes)
+
+                if key in counts:
+                    counts[key] += votes_int
+                else:
+                    counts[key] = votes_int
 
     return counts
 
 
 def get_rows(counts):
 
-    # you write this
+    rows = []
+    for key, votes in counts.items():
+        row = [*key, votes]
+        rows.append(row)
 
     return rows
 
 
 def sort_rows(rows):
 
-    # you write this
+    state_code_index = 1
+    votes_index = 3
+
+    rows_votes_ordered = sorted(rows, key=lambda row: row[votes_index], reverse=True)
+    rows_lex_ordered = sorted(rows_votes_ordered, key=lambda row: row[state_code_index])
 
     return rows_lex_ordered
 
 
 def write_rows(rows):
 
-    # you write this
-
+    with open(OUTPUT_PATH, "w+") as out_file:
+        csv_writer = csv.writer(out_file)
+        csv_writer.writerow(["year", "state_code", "candidate", "votes"])
+        csv_writer.writerow(rows)
 
 if __name__ == "__main__":
 
--- code/pandas_election_2020.py
+++ code/pandas_election_2020.py
@@ -6,4 +6,15 @@ OUTPUT_DIR = "artifacts"
 OUTPUT_PATH = os.path.join(OUTPUT_DIR, "election_report_pandas.csv")
 
 if __name__ == "__main__":
-    # you write this
+    (
+        pd.read_csv(IN_PATH)
+        .rename(columns={"state_po": "state_code", "candidatevotes": "votes"})
+        .dropna(subset=["candidatevotes"])
+        .assign(votes= lambda df: df["votes"].astype("int"))
+        .loc[lambda df: df["year" == 2020]]
+        .groupby(["year", "state_code", "candidate"])["votes"]
+        .sum()
+        .reset_index()
+        .sort_values(by=["state_code", "votes"], ascending= [True, False])
+        .to_csv(OUTPUT_PATH, index=False)
+    )
